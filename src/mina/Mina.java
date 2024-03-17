a8cc8b3a7b01769040979de61f540d5231a11b7e
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-03-17 08:28:14 +0800

    Mina

---
+++ src/mina/Mina.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mina;
+
+/**
+ *
+ * @author Rhodz
+ */
+public class Mina {
+    private String color, height, weight;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String color, height, weight;
+        
+        color = "Apricot";
+        height = "Mubo";
+        weight = "Tama-tama payat";
+        
+        System.out.println("Jihyo's favorite color is " + color + "." + 
+                "\nHer height and weight is " + height + " and " + weight) ;
+    }
+    
+}
